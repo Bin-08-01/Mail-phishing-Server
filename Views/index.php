8a1819019af89a146f99f5706d04be6610c63f00
Bin-08-01 <minhtri192035@gmail.com>
2022-10-29 00:07:22 +0700

    💬Minify CSS, change icon & name website

--- Views/index.php
+++ Views/index.php
@@ -6,12 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.ckeditor.com/4.19.1/standard/ckeditor.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="public/img/icon.ico" />
     <link rel="stylesheet" href="public/css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@200&family=Poppins:wght@100;300&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>Automatic Mail</title>
 </head>
 
 <body>
